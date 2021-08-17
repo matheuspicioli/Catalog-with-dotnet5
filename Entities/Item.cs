c398c52bfa27a69c51d085c05547e2568a09d394
Matheus Picioli <matheus.picioli98@gmail.com>
2021-08-16 22:51:10 -0300

    Fix Item entity modify set -> init only

--- Entities/Item.cs
+++ Entities/Item.cs
@@ -5,8 +5,8 @@ namespace Catalog.Entities
     public record Item
     {
         public Guid Id { get; init; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public DateTimeOffset CreatedDate { get; set; }
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
     }
 }
