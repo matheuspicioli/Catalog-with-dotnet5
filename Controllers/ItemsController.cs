604cd82691f4b509d98242efc7abdb5e0880696b
Matheus Picioli <matheus.picioli98@gmail.com>
2021-08-16 22:42:50 -0300

    Created controller, entity, repository (with Dependency Injection)

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
---
+++ Entities/Item.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Catalog.Entities
+{
+    public record Item
+    {
+        public Guid Id { get; init; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
