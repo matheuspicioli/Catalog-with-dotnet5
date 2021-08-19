eb5c68729a4d1af081d474649c81b8a7d0fbd66f
Matheus Picioli <matheus.picioli98@gmail.com>
2021-08-18 23:33:57 -0300

    Changing the repository mem -> mongo db

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Catalog.Repositories;
+using MongoDB.Driver;
+using Catalog.Settings;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson;
 
 namespace Catalog
 {
@@ -20,7 +25,16 @@ namespace Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+            services.AddSingleton<IMongoClient>(ServiceProvider =>
+            {
+                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                return new MongoClient(settings.ConnectionString);
+            });
+
+            services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
